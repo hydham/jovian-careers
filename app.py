d8649be4b6e2d027312194fd0281d40b1ce67b14
unknown <hydham007@gmail.com>
2024-11-14 19:32:36 +0530

    added home page

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+    {'title': 'Data engineer',
+     'location': 'Banglore, India',
+     'salary': 'Rs 10,00,000'},
+     {'title': 'Data scientist',
+     'location': 'Delhi, India',
+     'salary': 'Rs 10,00,000'},
+     {'title': 'Frontend Engineer',
+     'location': 'Remote',
+     'salary': '$ 60,000'},
+]
+
 @app.route("/")
 def home():
-    return "Welcome to Jovian Careers"
+    return render_template("home.html", jobs=JOBS)  
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
     app.run(debug=True)
