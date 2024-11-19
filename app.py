5b9526dc4a2efe1a8d195c28bfa1d01e65da4d52
hydham <hydham007@gmail.com>
2024-11-20 00:17:38 +0530

    added /job/id route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 import os
 from dotenv import load_dotenv
 
@@ -17,6 +17,13 @@ def home():
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
+
+@app.route("/job/<int:id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if job is None:
+        return jsonify({"message": "Job doesn't exist"})
+    return render_template('jobpage.html', job=job)
 
 if __name__ == "__main__":
     app.run(debug= os.getenv('FLASK_ENV') == 'development')
--- database.py
+++ database.py
@@ -10,11 +10,17 @@ def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT * FROM jobs"))
         jobs = [dict(row._mapping) for row in result.all()]
-        return jobs
+        return jobs 
 
-
-    
-
-
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"), 
+            {"val": id})
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(rows[0]._mapping)
 
 
